143e8a16c69f4e26651335b616e2bab65f1541c6
Monika Talwar <Monika.Talwar@in.tesco.com>
2016-07-28 08:21:11 +0530

    impl class changes

--- src/main/java/com/tesco/tps/service/CostAmendService.java
+++ src/main/java/com/tesco/tps/service/CostAmendService.java
@@ -2,13 +2,14 @@ package com.tesco.tps.service;
 
 import java.security.Principal;
 
+import javax.ws.rs.core.Response;
 import com.tesco.tps.core.service.GenericService;
 import com.tesco.tps.domain.CostAmendRequest;
-import com.tesco.tps.dto.CostAmendRequestDto;
-import com.tesco.tps.dto.CostAmendResponseDto;
+import com.tesco.tps.model.CostAmendInput;
 
 public interface CostAmendService extends GenericService<CostAmendRequest, String> {
 
-	void creatingCostDocument(CostAmendRequestDto costDocument, Principal principal);
+
+	Response creatingCostDocument(CostAmendInput costDocumentList, Principal userDetails);
 
 }
--- src/main/java/com/tesco/tps/service/impl/CostAmendServiceImpl.java
+++ src/main/java/com/tesco/tps/service/impl/CostAmendServiceImpl.java
@@ -1,12 +1,15 @@
 package com.tesco.tps.service.impl;
 
 import java.security.Principal;
-
+import java.util.List;
+import javax.ws.rs.core.Response;
 import com.google.inject.Inject;
+import com.tesco.tps.constant.CostAmendHttpResponse;
 import com.tesco.tps.core.annotation.Service;
 import com.tesco.tps.couchbase.service.AbstractCouchbaseService;
 import com.tesco.tps.domain.CostAmendRequest;
 import com.tesco.tps.dto.CostAmendRequestDto;
+import com.tesco.tps.dto.CostAmendResponseDto;
 import com.tesco.tps.dto.CostAmendResponseDto.MissingCostAmends;
 import com.tesco.tps.model.CostAmendInput;
 import com.tesco.tps.repository.CostAmendRepository;
@@ -15,38 +18,60 @@ import com.tesco.tps.service.CostAmendService;
 @Service
 public class CostAmendServiceImpl extends AbstractCouchbaseService<CostAmendRequest, String>
 		implements CostAmendService {
-    @Inject
+	@Inject
 	public CostAmendServiceImpl(CostAmendRepository repo) {
 		super(repo);
 	}
 
-	/*@Override
-	public void creatingCostDocument(CostAmendRequestDto costDocument, Principal userDetails) {
+	private static List<MissingCostAmends> missingList;
+
+	/*
+	 * @Override public void creatingCostDocument(CostAmendRequestDto
+	 * costDocument, Principal userDetails) {
+	 * costDocument.setCreatedBy(userDetails.getName());
+	 * costDocument.setModifiedBy(userDetails.getName()); try {
+	 * super.create(convert(costDocument, CostAmendRequest.class)); } catch
+	 * (RuntimeException exception) {
+	 * 
+	 * 
+	 * //convert(costDocument, MissingCostAmends.class); }
+	 * 
+	 * // costAmendResponseDto.setMissingSet(); }
+	 */
+	@Override
+	public Response creatingCostDocument(CostAmendInput costDocumentList, Principal userDetails) {
+		// List<MissingCostAmends> missingList = null;
+		CostAmendResponseDto costAmendResponseDto = new CostAmendResponseDto();
+		costDocumentList.getInputList().forEach(costDocument -> inseringAndSettingCostDocument(costDocument,
+				userDetails, costAmendResponseDto, missingList));
+		costAmendResponseDto.setMissingSet(missingList);
+		 return CostAmendHttpResponse.ok(costAmendResponseDto);
+	}
+
+	private void inseringAndSettingCostDocument(CostAmendRequestDto costDocument, Principal userDetails,
+			CostAmendResponseDto costAmendResponseDto, List<MissingCostAmends> missingList) {
 		costDocument.setCreatedBy(userDetails.getName());
 		costDocument.setModifiedBy(userDetails.getName());
 		try {
 			super.create(convert(costDocument, CostAmendRequest.class));
 		} catch (RuntimeException exception) {
-			
-			
-			//convert(costDocument, MissingCostAmends.class);
-		}
+			MissingCostAmends costAmendFailure = convert(costDocument, MissingCostAmends.class);
+			costAmendFailure.setReasonForFailure(exception.getMessage());
+			missingList.add(costAmendFailure);
 
-		// costAmendResponseDto.setMissingSet();
-	}*/
-    public void creatingCostDocument(CostAmendInput costDocumentList, Principal userDetails) {
-    	costDocumentList.getInputList().forEach(costDocument->{
-    		costDocument.setCreatedBy(userDetails.getName());
-    		costDocument.setModifiedBy(userDetails.getName());}
-		try {
-			super.create(convert(costDocument, CostAmendRequest.class));
-		} catch (RuntimeException exception) {
-			
-			
-			//convert(costDocument, MissingCostAmends.class);
 		}
+	}
+	/*
+	 * costDocumentList.getInputList().forEach(costDocument->{
+	 * costDocument.setCreatedBy(userDetails.getName());
+	 * costDocument.setModifiedBy(userDetails.getName()); } try {
+	 * super.create(convert(costDocument, CostAmendRequest.class)); } catch
+	 * (RuntimeException exception) {
+	 * 
+	 * 
+	 * //convert(costDocument, MissingCostAmends.class); }
+	 * 
+	 * // costAmendResponseDto.setMissingSet(); }
+	 */
 
-		// costAmendResponseDto.setMissingSet();
-    }
-    
 }
